24a6ef6cb2c20109b2e9436bd9915276478e5f34
Youssouf BOUDOUIA <yboudoui@made-f0Br2s3.clusters.42paris.fr>
2024-10-25 17:26:30 +0200

    testing

---
+++ P3/config/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo apt update
+sudo apt install ca-certificates curl -y
+
+# Variables
+SERVER_IP=$1
+
+if command -v kubectl &> /dev/null; then
+    echo "Kubectl installation found"
+else
+    curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+    sudo chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+    kubectl version --client
+    # Attendre que kubectl soit complètement installé
+    sleep 10
+fi
+
+if command -v docker &> /dev/null; then
+    echo "Docker installation found"
+else
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+fi
+
+# Installer K3d sur le serveur
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Attendre que K3d soit complètement installé
+sleep 10
--- P3/config/k3d-setup.sh
+++ P3/config/k3d-setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-sudo apt update
-sudo apt install ca-certificates curl -y
 
-# Variables
-SERVER_IP=$1
+# k3d cluster create mycluster --api-port 0.0.0.0:8080 --kubeconfig-update-default --kubeconfig-switch-context 
+k3d cluster create mycluster --kubeconfig-update-default --kubeconfig-switch-context 
+k3d kubeconfig merge mycluster --kubeconfig-switch-context
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+kubectl create namespace dev
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Installer K3d sur le serveur
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+# cp -r /vagrant/k3s-config /tmp/k3s
+# kubectl apply -f /tmp/k3s/ingress.yaml
 
-# Attendre que K3d soit complètement installé
-sleep 10
+# kubectl wait --for=condition=Ready -n argocd --all --timeout=15s
+
+
+# kubectl port-forward svc/argocd-server -n argocd 8080:443
+# argocd-server --insecure false
